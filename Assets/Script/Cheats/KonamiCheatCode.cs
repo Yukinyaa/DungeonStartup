5df944d4b71e7924864d3baa0a98ba49907ca80c
yukinyaa <yukinyaa712@gmail.com>
2020-12-02 23:04:00 +0900

    Added "cheat" codes

---
+++ Assets/Script/Cheats/KonamiCheatCode.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KonamiCheatCode : AbstractCheatCode
+{
+
+    protected override void OnActivateCheat()
+    {
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+    }
+
+
+    void Awake()
+    {
+        cheatCode = new KeyCode[] {
+            KeyCode.UpArrow,
+            KeyCode.UpArrow,
+            KeyCode.DownArrow,
+            KeyCode.DownArrow,
+            KeyCode.LeftArrow,
+            KeyCode.RightArrow,
+            KeyCode.LeftArrow,
+            KeyCode.RightArrow,
+            KeyCode.B,
+            KeyCode.A
+        };
+    }
+}
---
+++ Assets/Script/Cheats/ShiawaninaruKakushiCommandCode.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShiawaninaruKakushiCommandCode : AbstractCheatCode
+{
+
+    protected override void OnActivateCheat()
+    {
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["광선검"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+        InventoryManager.Instance.AddToInventory(ItemDB.ItemDic["역장 생성기"]);
+      
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+
+
+    void Awake()
+    {
+        cheatCode = new KeyCode[] {
+            KeyCode.RightArrow,
+            KeyCode.DownArrow,
+            KeyCode.UpArrow,
+            KeyCode.RightArrow,
+
+            KeyCode.RightArrow,
+            KeyCode.DownArrow,
+            KeyCode.RightArrow,
+            KeyCode.RightArrow,
+
+            KeyCode.UpArrow,
+            KeyCode.UpArrow,
+            KeyCode.DownArrow,
+            KeyCode.DownArrow,
+
+            KeyCode.LeftArrow,
+            KeyCode.RightArrow,
+            KeyCode.LeftArrow,
+            KeyCode.RightArrow,
+
+
+
+            KeyCode.R,
+            KeyCode.E,
+            KeyCode.S,
+            KeyCode.E,
+            KeyCode.T,
+        };
+    }
+}
